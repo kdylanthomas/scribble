81ca29e5f007fdefbb5d86a3ecdb0809bd4332ff
Dylan Thomas <kdylanthomas@gmail.com>
2016-06-16 15:46:32 -0500

    using oauth to retrieve user data from Google

--- app/controllers/LoginCtrl.js
+++ app/controllers/LoginCtrl.js
@@ -1,5 +1,20 @@
 'use strict';
 
-app.controller("LoginCtrl", function ($scope) {
-	console.log('a controller!');
-});
+app.controller('LoginCtrl', [
+	'$scope',
+	'$http',
+	//'AuthFactory',
+	function ($scope, $http) {
+
+		$scope.googleOauth = function () {
+			OAuth.initialize('VQpWdUCqvJVMusQiRHOgoExz274')
+			OAuth.popup('google').done(function(result) {
+		    console.log(result)
+		    // do some stuff with result
+		    result.me().done(data => {
+		    	console.log(data);
+    		});
+			});
+		}
+	}
+]);
