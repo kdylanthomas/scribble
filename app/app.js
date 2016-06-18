35181f9773193158483c8a56f6ac3666e0f74bff
dylan thomas <kdylanthomas@gmail.com>
2016-06-18 15:44:41 -0500

    successfully posting entries to db; running into CORS issues with external API

--- app/app.js
+++ app/app.js
@@ -1,19 +1,24 @@
 'use strict';
 
 let app = angular.module('scribble', ['ngRoute'])
-.constant('journalServer', 'http://localhost:5000/api/');
+.constant('journalServer', 'http://localhost:5000/api/')
+.constant('indicoSentiment', 'https://apiv2.indico.io/sentiment?key=43ef4ee1b5a5375de5a3e17f9d69794f')
+.constant('indicoEmotion', 'https://apiv2.indico.io/emotion?key=43ef4ee1b5a5375de5a3e17f9d69794f');
 
+app.config(['$routeProvider', '$httpProvider',
 
-app.config(['$routeProvider',
-  ($routeProvider) => {
+  ($routeProvider, $httpProvider) => {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    
     $routeProvider.
       when('/login', {
         templateUrl: 'partials/login-view.html',
         controller: 'LoginCtrl'
       }).
       when('/journal', {
-        templateUrl: 'partials/journal-view.html'
-        //controller: 'JournalCtrl'
+        templateUrl: 'partials/journal-view.html',
+        controller: 'JournalCtrl'
       }).
       otherwise({
         redirectTo: '/login'
--- app/controllers/JournalCtrl.js
+++ app/controllers/JournalCtrl.js
@@ -3,6 +3,12 @@
 // then, make post request to indico /sentiment and /emotion for data
 // use those responses to build a entryAnalysis object and POST to db
 
+// 1. POST entry. on success, trigger GET 
+// 2. GET entry so you have the entryId  <-- get request should return a promise
+// 3. POST to indico /sentiment
+// 4. POST to indico /emotion
+// 5. POST entryAnalysis object to db 
+
 'use strict';
 
 app.controller('JournalCtrl', [
@@ -10,8 +16,64 @@ app.controller('JournalCtrl', [
 	'authenticate',
 	'$http',
 	'$q',
-	function ($scope, authenticate, $http, $q) {
-		let currentUser = authenticate.getUser();
-		console.log(currentUser);
+	'journalServer',
+	'indicoEmotion',
+	'indicoSentiment',
+	function ($scope, authenticate, $http, $q, journalServer, indicoEmotion, indicoSentiment) {
+		//$scope.currentUser = authenticate.getUser(); // get user currently logged in
+
+		$scope.journalEntry = {
+			text: '',
+			dateStarted: null,
+			dateSubmitted: null,
+			wordCount: 0,
+			//userId: $scope.currentUser.UserId
+			userId: 5
+		}
+
+		$scope.entryAnalysis = {
+			anger: 0,
+			joy: 0,
+			fear: 0,
+			surprise: 0,
+			sadness: 0,
+			sentiment: 0
+		};
+
+		let startedWriting = false;
+
+		$scope.assignDateStarted = function () { // method on $scope to use ng-keypress directive
+			if (!startedWriting) {
+				$scope.journalEntry.dateStarted = new Date();
+				startedWriting = true;
+			}
+		}
+
+		$scope.postEntry = function () {
+			$scope.journalEntry.dateSubmitted = new Date();
+			$scope.journalEntry.wordCount = getWordCount($scope.journalEntry.text);
+			$http.post(`${journalServer}Entry`, JSON.stringify($scope.journalEntry))
+			.then(
+				res => {
+					let submittedEntry = res.data;
+					console.log('successfully saved entry!', submittedEntry);
+					analyzeSentiment(submittedEntry.text);
+				},
+				err => console.log('Something went wrong:', err) // err
+			);
+		}
+
+		let getWordCount = (text) => text.split(' ').length
+
+		let analyzeSentiment = (text) => {
+			$http.post(indicoSentiment, {'data': text})
+			.then(
+				res => console.log('success!', res),
+				err => console.log('error :(', err)
+			)
+		}
+
+		$scope.analyzeEmotion = function (text) {}
+
 	}
 ]);
